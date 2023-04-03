bd8fd9b31a3564ffca597f9e207a42a27752106e
YuhaoWU <wuyuhao2019@126.com>
2023-04-03 12:18:31 +0800

    add func sql gene

--- benchmark.py
+++ benchmark.py
@@ -44,6 +44,51 @@ def timing_function(some_function):
         print(f"for function {some_function.__name__}, execution time: {end_time - start_time:.10f} seconds")
     return wrapper
 
+# 对函数执行，采样 Time-based memory usage + 获取max memory usage
+def get_func_max_memory(sql_func, viz_plot=False):
+    
+    filename = sql_func.__name__ + "_memory"
+    cmd = ["mprof", "run", "-o", filename + ".dat", "python", "-c", f"import {sql_func.__module__}; \
+                {sql_func.__module__}.{sql_func.__name__}()"]
+    subprocess.run(cmd) 
+    # memory = subprocess.run(cmd, capture_output=True, text=True)
+    # memory = subprocess.check_output(cmd)
+
+    # 获取memory最后值，或者 取max峰值
+    dat_file = filename + ".dat"
+    num_lines = 5
+
+    with open(dat_file, "r") as f:
+        # 使用切片操作获取最后几行
+        last_lines = f.readlines()[-num_lines:]
+        '''
+        .dat format like this:
+        MEM 37.640625 1680389460.7517
+        MEM 47.265625 1680389460.8554
+        MEM 58.031250 1680389460.9559
+        MEM 59.214844 1680389461.0603
+        '''
+        memory_samples = [round(float(line.split(" ")[1]),4) for line in last_lines]
+    # print(memory_samples)
+    
+    
+    # 使用 subprocess 模块启动新的进程执行 mprof plot 命令, 绘图
+    if viz_plot:
+        
+        plot_cmd = ["mprof", "plot", "-o", f"{filename}.png", "-t", f"time-based {filename}", dat_file]
+        subprocess.run(plot_cmd)
+    
+    # 清除当前文件夹下的 生成memory图的.dat
+    subprocess.run(["mprof", "clean"])
+        
+    return max(memory_samples)
+
+# 生成对应的sql, generation
+def sqlalch_gen_sql(sql_func, db_backend=mysql):
+    
+    query = sql_func()
+    return str(query.statement.compile(dialect=db_backend.dialect()))
+
 
 def sqlalch_insert_benchmark():
     pass
@@ -101,53 +146,32 @@ def sqlalch_query_benchmark():
     # sqlalch_query_benchmark_time(func_list)
     sqlalch_query_benchmark_memory(func_list)
 
-# 对函数执行，采样 Time-based memory usage + 获取max memory usage
-def get_func_max_memory(sql_func, viz_plot=False):
-    
-    filename = sql_func.__name__ + "_memory"
-    cmd = ["mprof", "run", "-o", filename + ".dat", "python", "-c", f"import {sql_func.__module__}; \
-                {sql_func.__module__}.{sql_func.__name__}()"]
-    subprocess.run(cmd) 
-    # memory = subprocess.run(cmd, capture_output=True, text=True)
-    # memory = subprocess.check_output(cmd)
 
+def sqlalch_sql_generation():
+    func_list = [
+        {'name': '1', 'func': sql1},
+        {'name': '2', 'func': sql2},
+        {'name': '4', 'func': sql4},
+        {'name': '5', 'func': sql5},
+        {'name': '8', 'func': sql8},
+        {'name': '9', 'func': sql9},
+        {'name': '10', 'func': sql10},
+        {'name': '11', 'func': sql11},
+        {'name': '12', 'func': sql12},
+        {'name': '13', 'func': sql13},
+        {'name': '15', 'func': sql15},
+        {'name': '16', 'func': sql16},
+    ]
+    path = './orm_generate_sql.txt'
+    with open(path, 'w', encoding='utf-8') as f:
+        for func in func_list:
+            # json.dump(func['name'], fp)
+            # f.write('\n')
+            json.dump("sql"+func['name'] + ": " + sqlalch_gen_sql(func['func']), f)
+            f.write('\n\n')
     
-    # 获取memory最后值，或者 取max峰值
-    dat_file = filename + ".dat"
-    num_lines = 5
-
-    with open(dat_file, "r") as f:
-        # 使用切片操作获取最后几行
-        last_lines = f.readlines()[-num_lines:]
-        '''
-        .dat format like this:
-        MEM 37.640625 1680389460.7517
-        MEM 47.265625 1680389460.8554
-        MEM 58.031250 1680389460.9559
-        MEM 59.214844 1680389461.0603
-        '''
-        memory_samples = [round(float(line.split(" ")[1]),4) for line in last_lines]
-    # print(memory_samples)
-    
-    
-    # 使用 subprocess 模块启动新的进程执行 mprof plot 命令, 绘图
-    if viz_plot:
         
-        plot_cmd = ["mprof", "plot", "-o", f"{filename}.png", "-t", f"time-based {filename}", dat_file]
-        subprocess.run(plot_cmd)
-    
-    # 清除当前文件夹下的 生成memory图的.dat
-    subprocess.run(["mprof", "clean"])
-        
-    return max(memory_samples)
-
-
-
-# 生成对应的sql, generation
-def sqlalch_gen_sql(sql_func, db_backend=mysql):
-    
-    query = sql_func()
-    return str(query.statement.compile(dialect=db_backend.dialect()))
 
 if __name__ == '__main__':
-    sqlalch_query_benchmark()
+    # sqlalch_query_benchmark()
+    sqlalch_sql_generation()
